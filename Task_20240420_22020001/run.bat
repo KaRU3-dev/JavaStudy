b5edcd02b2ae2af3c2a47ad4e4e186a121c0b473
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2024-04-20 17:51:29 +0900

    Update run.bat

--- Task_20240420_22020001/run.bat
+++ Task_20240420_22020001/run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem JDK 21.0.2
-set JAVA_HOME="C:\Users\himar\Desktop\InDesktop\jdks\jdk-21.0.2\bin"
+set JAVA_HOME="/path/to/jdk-{version}/bin"
 set JAR="Task_20240420_22020001.jar"
 
 %JAVA_HOME%\java -jar -Xms1024m -Xms1024m %JAR%
