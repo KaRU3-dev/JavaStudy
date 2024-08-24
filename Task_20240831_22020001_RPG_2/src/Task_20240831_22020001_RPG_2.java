1a2929a6cc5d4cc6b9be100c5b52f1dfad223dc9
KaRU3 <121723731+KaRU3-dev@users.noreply.github.com>
2024-08-24 00:04:18 +0000

    Update main class name to project name.

--- Task_20240831_22020001_RPG_2/src/Task_20240831_22020001_RPG_2.java
+++ Task_20240831_22020001_RPG_2/src/Task_20240831_22020001_RPG_2.java
@@ -1,7 +1,7 @@
 import game.GameDifficulty;
 import game.GameSystem;
 
-public class App {
+public class Task_20240831_22020001_RPG_2 {
     public static void main(String[] args) throws Exception {
         // Create game system
         GameSystem gameSystem = new GameSystem(GameDifficulty.NORMAL);
