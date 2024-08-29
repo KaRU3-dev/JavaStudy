99df59a8928a27aa730761323ede87cfd63cbca5
KaRU3-dev <121723731+KaRU3-dev@users.noreply.github.com>
2024-08-29 13:23:42 +0900

    Fix runtime error for can not load main classfile in jar.

--- Task_20240831_22020001_RPG_2/run.bat
+++ Task_20240831_22020001_RPG_2/run.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem THIS FILE IS ONLY FOR WINDOWS
+
 rem Execute config
 set MIN_MEMORY=512m
 set MAX_MEMORY=1024m
@@ -7,7 +9,7 @@ echo MIN_MEMORY: %MIN_MEMORY%
 echo MAX_MEMORY: %MAX_MEMORY%
 
 rem JDK Path
-set JAVA_HOME=/path/to/jdks/jdk-21.0.2/bin
+set JAVA_HOME=/path/to/java/bin
 echo JAVA_HOME: %JAVA_HOME%
 
 rem Jar file path
@@ -15,7 +17,9 @@ set JAR_FILE_PATH=Task_20240831_22020001_RPG_2.jar
 echo JAR_FILE_PATH: %JAR_FILE_PATH%
 
 rem Run the jar file
-%JAVA_HOME%/java -Xms%MIN_MEMORY% -Xmx%MAX_MEMORY% -jar %JAR_FILE_PATH%
+rem CAUTION: jdk 22 or later is required to use --enable-preview
+rem CAUTION: I tested with only jdk 22.0.2
+%JAVA_HOME%\java --enable-preview -Xms%MIN_MEMORY% -Xmx%MAX_MEMORY% -jar %JAR_FILE_PATH%
 echo End
 
 pause
